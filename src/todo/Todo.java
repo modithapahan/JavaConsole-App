7196deef94116b1d23cd30053bdc49754b7cfe46
modithapahan <modithapahan0@gmail.com>
2023-08-23 22:50:49 +0530

    todo

--- src/todo/Todo.java
+++ src/todo/Todo.java
@@ -13,31 +13,67 @@ public class Todo {
 
         System.out.println("");
 
-        /* Any String */
-        String key = "kbhcbyubb";
-
         System.out.println("Do you want to continue? (Y/N)");
         String answer = input.nextLine();
 
-        while (answer.equals("y") || answer.equals("Y") || answer.equals("n") || answer.equals("N")) {
+        if (answer.equals("y") || answer.equals("Y") || answer.equals("n") || answer.equals("N")) {
 
             if (answer.equals("y") || answer.equals("Y")) {
-                System.out.println("Enter your task");
+
+                System.out.println("");
+                System.out.println("=================================");
+
+                System.out.println("Enter a test word: ");
                 String task = input.nextLine();
-            } else if (answer.equals("n") || answer.equals("N")) {
+
+                System.out.println("");
+                System.out.println("==== -G R A T E- ====");
+                System.out.println("");
+
+                if (task.length() >= 100) {
+                    System.out.println("Too much!");
+                } else {
+                    int x = 1;
+                    String newOne = "";
+
+                    while (x < 3) {
+                        System.out.println("Enter your task: ");
+                        task = input.nextLine();
+
+                        newOne += task;
+                        x++;
+                    }
+
+                    System.out.println("");
+
+                    System.out.println("" + newOne);
+
+                    System.out.println("");
+
+                    System.out.println("=================================");
+                    System.out.println("Bye, Have a nice day!");
+                    System.out.println("=================================");
+
+                    return;
+                }
+
+            }
+
+            else {
                 System.out.println("=================================");
                 System.out.println("Bye, Have a nice day!");
                 System.out.println("=================================");
-            } else {
-                System.out.println("=================================");
-                System.out.println("oops, Invalid charators! Try again!");
-                System.out.println("=================================");
+
+                return;
             }
         }
 
-        System.out.println("=================================");
-        System.out.println("oops, Invalid charators! Try again!");
-        System.out.println("=================================");
-        return;
+        else {
+            System.out.println("=================================");
+            System.out.println("oops, Invalid charators! Error!");
+            System.out.println("=================================");
+
+            return;
+        }
     }
 }
